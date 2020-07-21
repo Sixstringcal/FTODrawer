90ac441f3d1388d4b88a139d3579a961afe208d5
Calvin Nielson <sixstringcal@gmail.com>
2020-07-20 19:46:44 -0600

    Rs, Rw, and BLs Moves defined

--- src/FTO.java
+++ src/FTO.java
@@ -14,47 +14,87 @@ public class FTO {
         String[] moves = scramble.split(" ");
         for (int i = 0; i < moves.length; i++) {
             switch (moves[i]) {
+
+                /**
+                 * R-based moves and rotations
+                 */
                 case "R":
                     doR(1);
                     break;
                 case "R'":
                     doR(2);
                     break;
+                case :
+                    "BLs'":
+                case "Rs":
+                    doRs(1);
+                    break;
+                case "BLs":
+                case "Rs'":
+                    doRs(2);
                 case "Rw":
-                    //Do Rw move
+                    doR(1);
+                    doRs(1);
                     break;
                 case "Rw'":
-                    //Do Rw' move
+                    doR(2);
+                    doRs(2);
                     break;
                 case "Ro":
-                    //Do Ro rotation
+                    //TODO: Ro rotation
                     break;
                 case "Ro'":
-                    //Do Ro' rotation
+                    //TODO: Ro' rotation
                     break;
+
+                /**
+                 * U-based moves and rotations
+                 */
                 case "U":
-                    //Do U move
+                    //TODO: U move
                     break;
                 case "U'":
-                    //Do U' move
+                    //TODO: U' move
+                    break;
+                case "Ds'":
+                case "E'":
+                case "Us":
+                    //TODO: Us move
+                    break;
+                case "Ds":
+                case "E":
+                case "Us'":
+                    //TODO: Us' move
                     break;
                 case "Uw":
-                    //Do Uw move
+                    //TODO: Uw move
                     break;
                 case "Uw'":
-                    //Do Uw' move
+                    //TODO: Uw' move
                     break;
                 case "Uo":
-                    //Do Uo rotation
+                    //TODO: Uo rotation
                     break;
                 case "Uo'":
-                    //Do Uo' rotation
+                    //TODO: Uo' rotation
                     break;
+
+                /**
+                 * L-based moves and rotations
+                 */
                 case "L":
                     //Do L move
                     break;
                 case "L'":
                     //Do L' move
+                    break;
+                case "BRs'":
+                case "Ls":
+                    //Do Ls move
+                    break;
+                case "BRs":
+                case "Ls'":
+                    //Do Ls' move
                     break;
                 case "Lw":
                     //Do Lw move
@@ -68,11 +108,25 @@ public class FTO {
                 case "Lo'":
                     //Do Lo' rotation
                     break;
+
+                /**
+                 * F-based moves and rotations
+                 */
                 case "F":
                     //Do F move
                     break;
                 case "F'":
                     //Do F' move
+                    break;
+                case "Bs'":
+                case "S":
+                case "Fs":
+                    //Do Fs move
+                    break;
+                case "Bs":
+                case "S'":
+                case "Fs'":
+                    //Do Fs' move
                     break;
                 case "Fw":
                     //Do Fw move
@@ -86,6 +140,10 @@ public class FTO {
                 case "Fo'":
                     //Do Fo' rotation
                     break;
+
+                /**
+                 * D-based moves and rotations
+                 */
                 case "D":
                     //Do D move
                     break;
@@ -104,6 +162,10 @@ public class FTO {
                 case "Do'":
                     //Do Do' rotation
                     break;
+
+                /**
+                 * B-based moves and rotations
+                 */
                 case "B":
                     //Do B move
                     break;
@@ -122,6 +184,10 @@ public class FTO {
                 case "Bo'":
                     //Do Bo' rotation
                     break;
+
+                /**
+                 * BR-based moves and rotations
+                 */
                 case "BR":
                     //Do BR move
                     break;
@@ -140,6 +206,10 @@ public class FTO {
                 case "BRo'":
                     //Do BRo' rotation
                     break;
+
+                /**
+                 * BL-based moves and rotations
+                 */
                 case "BL":
                     //Do BL move
                     break;
@@ -158,6 +228,7 @@ public class FTO {
                 case "BLo'":
                     //Do BLo' rotation
                     break;
+                    
                 default:
                     throw new IllegalArgumentException("The move " + moves[i] + " is not supported.");
             }
@@ -221,6 +292,47 @@ public class FTO {
             state[30] = state[28];
             state[28] = state[33];
             state[33] = temp;
+        }
+    }
+
+    public void doRs(int times) {
+        int temp;
+        for (int i = 0; i < times; i++) {
+            //UB Edges
+            temp = state[2];
+            state[2] = state[19];
+            state[19] = state[42];
+            state[42] = temp;
+
+            //BU Edges
+            temp = state[47];
+            state[47] = state[16];
+            state[16] = state[70];
+            state[70] = temp;
+
+            //UL Edges
+            temp = state[5];
+            state[5] = state[24];
+            state[24] = state[39];
+            state[39] = temp;
+
+            //LU Edges
+            temp = state[11];
+            state[11] = state[65];
+            state[65] = state[50];
+            state[50] = temp;
+
+            //U Triangles
+            temp = state[1];
+            state[1] = state[23];
+            state[23] = state[43];
+            state[43] = temp;
+
+            //L Triangles
+            temp = state[12];
+            state[12] = state[66];
+            state[66] = state[46];
+            state[46] = temp;
         }
     }
 
