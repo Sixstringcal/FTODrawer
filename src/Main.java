dd85cdab62a13bc6bac7c8024d81b5da82e9b183
Calvin Nielson <sixstringcal@gmail.com>
2020-07-22 16:32:10 -0600

    color scheme is editable through colors.txt now.  It also won't crash if it's gone supposedly.  idk haven't tested it

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,6 +11,16 @@ public class Main {
     };
 
     public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("colors.txt"));
+            int i = 0;
+            while(scanner.hasNextLine()){
+                colors[i] = scanner.nextLine();
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         MainMenu mainMenu = new MainMenu();
 
     }
